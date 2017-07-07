afd23cf85411c65a30b588aeb3d787670b1dfc4b
liang-silin <liang-silin@hotmail.com>
2017-07-07 14:41:27 +0800

    DrawerLayout

--- app/src/main/java/com/jbwl/materialdesign/MainActivity.java
+++ app/src/main/java/com/jbwl/materialdesign/MainActivity.java
@@ -1,5 +1,8 @@
 package com.jbwl.materialdesign;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,12 +12,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
     }
 
     @Override
@@ -37,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.settings1:
                 Toast.makeText(this, "Settings1", Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
         }
