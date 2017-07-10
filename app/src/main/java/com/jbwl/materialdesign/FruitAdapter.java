c7f0e5285d8b9dd5247ab4211fc935ba8892afd3
liang-silin-company <liang-silin@hotmail.com>
2017-07-10 16:15:04 +0800

    CollapsingToolbarlayout

--- app/src/main/java/com/jbwl/materialdesign/FruitAdapter.java
+++ app/src/main/java/com/jbwl/materialdesign/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.jbwl.materialdesign;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -30,7 +31,21 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
         }
 
         View view = LayoutInflater.from(context).inflate(R.layout.fruit_item, parent, false);
-        return new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = fruitList.get(position);
+                Intent intent = new Intent(context, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                context.startActivity(intent);
+            }
+        });
+        return holder;
     }
 
     @Override
